0cb48b72db7f9bb9359b8536fc01d230a3aea074
PetrasViz <x10kuliai36@gmail.com>
2018-05-29 07:59:00 +0300

    Ikelimas

--- Namu darbas nr10/js/script.js
+++ Namu darbas nr10/js/script.js
@@ -1,13 +1,17 @@
 // JavaScript source code
 document.getElementById("submit").addEventListener("click", skaiciavimas);
 function skaiciavimas() {
-    var h = parseFloat(document.getElementById("aukstis").value);
-    var w = parseFloat(document.getElementById("plotis").value);
-    var option = document.getElementsByName("pasirinkimas");
+    var h = parseFloat(document.getElementById("aukstis").value); //aukscio reiksme
+    var w = parseFloat(document.getElementById("plotis").value); //plocio reiksme
+    if (h < 0 || w < 0) {
+        alert("?veskite teigiam? reikšm?"); //Patikrinam ar ivesta reiksme teigiama ar ne, jei neigiama nutraukiam funkcija.
+        return;
+    }
+    var option = document.getElementsByName("pasirinkimas"); //gauname radio reiksmes
     var checkedValue;
     for (var i = 0; i < option.length; i++) {
         if (option[i].checked) {
-            checkedValue = option[i].value;
+            checkedValue = option[i].value; //tikriname visas gauto masyvo reiksmes ar pazymeta, jei pazymeta priskiriame prie kintamojo 
         }
     }
     switch (checkedValue) {
